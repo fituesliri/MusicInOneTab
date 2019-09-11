3b3fc1305f3bea74c40d336748f24e2dab9eec35
Jiyeong Park <jiypark@e1z3r5p10.42.us.org>
2019-09-11 00:02:24 -0700

    rectangle

--- index.js
+++ index.js
@@ -2,31 +2,27 @@ window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector('.visual');
+    // const button = document.querySelector(".button");
     const colors = [
         "#89ec89",
         "#f17474",
         "#ea82ff",
         "#e9e76e",
         "#8c27eb",
-        "#5997d1"
+        "#0a4175"
     ]
-    //lets start recording
-    record.onclick = e => {
-        console.log('I was clicked')
-        record.disabled = true;
-        record.style.backgroundColor ="#8c27eb";
-        stopRecord.disabled=false;
-        audioChunks = [];
-        rec.start
-    }
-    //lets stop recording
-    stopRecord.onclick = e => {
-        console.log("I was clicked")
-        record.disabled = false;
-        stop.disabled=true;
-        record.style.backgroundclolor = "#89ec89";
-    }
+    // //lets start recording
+    // button.onclick = e => {
+    //     console.log('I was clicked')
+    //     function mutePage() {
+    //     var elems = document.querySelectorAll("video, audio");
+    //    [].forEach.call(elems, function(elem) { muteMe(elem); });
+    //     button.backgroundColor ="#8c27eb";
+    //     button.disabled=false;
+    // }
+
     //lets get going with the sound
+
     pads.forEach((pad, index) =>{
         pad.addEventListener("click", function(){
             sounds[index].currentTime = 0;
